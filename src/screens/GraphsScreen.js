cf14d914ebd0391e9fd2b35d558a1d1912f1227e
LuisBrodely <luisbrodely2@gmail.com>
2023-06-02 05:59:10 -0600

    Graficas

--- src/screens/GraphsScreen.js
+++ src/screens/GraphsScreen.js
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { Text, StatusBar, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import Donut from '../components/Graphs/Donut';
+
+const data = [{
+  percentage: 8,
+  color: 'tomato',
+  max: 100, 
+  radius: 100
+}, {
+  percentage: 14,
+  color: 'skyblue',
+  max: 100
+}, {
+  percentage: 20,
+  color: 'gold',
+  max: 100
+}, {
+  percentage: 20,
+  color: '#222',
+  max: 100
+}]
 
 export default function GraphsScreen() {
   return (
-    <View>
-      <Text>GraphsScreen</Text>
+    <View style={styles.container}>
+      <StatusBar hidden/>
+      <View style={{flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center'}}>
+        {data.map((p, i) => {
+          return <Donut key={i} percentage={p.percentage} color={p.color} delay={500 + 100 * i} max={p.max} radius={p.radius}/>
+        })}
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
